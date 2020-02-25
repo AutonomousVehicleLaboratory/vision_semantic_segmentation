771a8f9d2e1886505d1af786bd610ee3569d1884
ZHYHenryZhang <henryzhang19951213@gmail.com>
2020-02-25 09:43:37 -0800

    framwork

--- src/mapping.py
+++ src/mapping.py
@@ -7,12 +7,17 @@ Date:February 24, 2020
 
 # module
 import numpy as np
-from homography import generate_homography
+import cv2
+
 import rospy
 from tf import Transformer, TransformListener, TransformBroadcaster, LookupException, ConnectivityException, ExtrapolationException, TransformerROS
-from tf.transformations import quaternion_matrix
+from tf.transformations import quaternion_matrix, euler_from_quaternion
 from geometry_msgs.msg import PoseStamped, TransformStamped
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
 
+from camera import camera_setup_6
+from homography import generate_homography
 # parameters
 
 
@@ -20,6 +25,8 @@ from geometry_msgs.msg import PoseStamped, TransformStamped
 class SemanticMapping:
     def __init__(self, discretization = 0.1, boundary = [[0, 40], [-10, 10]]):
         self.sub_pose = rospy.Subscriber("/current_pose", PoseStamped, self.pose_callback)
+        # self.image_sub_cam6 = rospy.Subscriber("/camera6/image_raw", Image, self.image_callback)
+
         self.tf_listener_ = TransformListener()
         self.br = TransformBroadcaster()
         self.tfmr = Transformer()
@@ -30,6 +37,8 @@ class SemanticMapping:
         self.map = np.zeros((self.x_max, self.y_max))
         self.map_pose = None
         self.pose = None
+        self.cam6 = camera_setup_6()
+        self.bridge = CvBridge()
     
     def pose_callback(self, msg):
         self.pose = msg.pose
@@ -38,7 +47,8 @@ class SemanticMapping:
             self.set_map_pose(self.pose)
         else:
             self.set_map_pose(self.map_pose)
-            self.calculate_transformation(msg)
+            self.get_local_transformation()
+            
 
     def set_map_pose(self, pose):
         m = TransformStamped()
@@ -54,7 +64,7 @@ class SemanticMapping:
         m.transform.rotation.w = pose.orientation.w
         self.br.sendTransformMessage(m)
     
-    def calculate_transformation(self, pose):
+    def get_local_transformation(self):
         try:
             (trans, rot) = self.tf_listener_.lookupTransform('/local_map', '/base_link', rospy.Time(0))
         except (LookupException, ConnectivityException, ExtrapolationException):
@@ -63,47 +73,63 @@ class SemanticMapping:
         # tf_pose = self.tf_listener_.transformPose("/world", pose)
         # R_local = quaternion_matrix(tf_pose.pose.orientation)
         T = self.tf_ros.fromTranslationRotation(trans, rot)
+        euler = euler_from_quaternion(rot)
         
-        print "Position of the pose in the local map:"
-        print T
+        # print "Position of the pose in the local map:"
+        print trans, rot, euler
+        return T, trans, rot, euler
         
+    def image_callback(self, msg):
+        try:
+            image_in = self.bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
+        except CvBridgeError as e:
+            print(e)
+        
+        ## ========== Image preprocessing
+        image_in = cv2.cvtColor(image_in, cv2.COLOR_BGR2RGB)
+        self.mapping(image_in, self.pose)
 
-    # def mapping(self, im_src, pose):
-    #     self.pose = pose
-    #     if self.require_new_map(pose):
-    #         self.map = self.transform_old_map( self.pose, pose)
+    def mapping(self, im_src, pose):
+        self.pose = pose
+        if self.require_new_map(pose):
+            self.map = self.transform_old_map( self.map_pose, pose)
+            self.map_pose = pose
+            self.set_map_pose(self.pose)
 
-    #     """ Take in image, add semantic information to the local map """
-    #     im_dst = self.transform_mask(im_src, pose)
-    #     updated_map = self.update_map(im_dst)
-    #     self.map = updated_map
+        """ Take in image, add semantic information to the local map """
+        im_dst = self.transform_mask(im_src, pose)
+        updated_map = self.update_map(im_dst)
+        self.map = updated_map
 
-    # def require_new_map(self, pose):
-    #     if np.abs(pose.position.x - self.map_pose.position.x) > 10 or 
-    #        np.abs(pose.position.y - self.map_pose.position.x)
+    def require_new_map(self, pose):
+        transform_matrix, trans, rot, euler = self.get_local_transformation()
+        if np.abs(trans[0]) > 10 or np.abs(trans[1]) > 2:
+            return True
+        else:
+            return False
 
-    # def transform_old_map(self, old_pose, new_pose):
-    #     pass
+    def transform_old_map(self, old_pose, new_pose):
+        pass
     
-    # def transform_mask(self, im_src, pose):
-    #     # prepare
+    def transform_mask(self, im_src, pose):
+        """ retrive map mask from current image """
+        # prepare
 
-    #     pts_src = None
-    #     pts_dst = None
+        im_dst = im_src
+        # transform
+        # homography approach might be not suitable for non planar approach
+        # im_dst = generate_homography(im_src, pts_src, pts_dst)
+        return im_dst
 
-    #     # transform
-    #     im_dst = generate_homography(im_src, pts_src, pts_dst)
-    #     return im_dst
-
-    # def update_map(self, im_dst):
-    #     log_odds_map = self.update_log_odds(im_dst)
-    #     return log_odds_map
+    def update_map(self, im_dst):
+        log_odds_map = self.update_log_odds(im_dst)
+        return log_odds_map
     
-    # def update_log_odds(self, im_dst):
-    #     pass
+    def update_log_odds(self, im_dst):
+        pass
 
-    # def binarize(self, map_in):
-    #     return map_in
+    def binarize(self, map_in):
+        return map_in
 # functions
 
 
