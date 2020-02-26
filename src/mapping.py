db0d842bc798293d249c99174b63dbcae93c4dcd
ZHYHenryZhang <henryzhang19951213@gmail.com>
2020-02-26 09:56:31 -0800

    adjusted map boundary

--- src/mapping.py
+++ src/mapping.py
@@ -28,7 +28,7 @@ from utils_ros import set_map_pose, get_transformation, get_transform_from_pose
 
 # classes
 class SemanticMapping:
-    def __init__(self, discretization = 0.1, boundary = [[0, 40], [-10, 10]]):
+    def __init__(self, discretization = 0.1, boundary = [[10, 50], [-10, 10]]):
         self.sub_pose = rospy.Subscriber("/current_pose", PoseStamped, self.pose_callback)
         self.sub_pcd = rospy.Subscriber("/reduced_map", PointCloud2, self.pcd_callback)
         self.image_sub_cam6 = rospy.Subscriber("/camera6/image_raw", Image, self.image_callback)
@@ -36,39 +36,47 @@ class SemanticMapping:
         self.tf_listener_ = TransformListener()
         self.tfmr = Transformer()
         self.tf_ros = TransformerROS()
+        self.bridge = CvBridge()
         
-        self.map_pose = None
         self.pose = None
         self.cam6 = camera_setup_6()
-        self.bridge = CvBridge()
         self.pcd = None
-
-        self.boundary = boundary
+        
+        self.map = None
+        self.map_pose = None
+        self.map_boundary = boundary
         self.d = discretization # discretization in meters
         self.map_width = int((boundary[0][1] - boundary[0][0]) / self.d)
         self.map_height = int((boundary[1][1] - boundary[1][0]) / self.d)
         self.map_depth = 3
-        self.map = None
         self.map_value_max = 10 # prevent over confidence
+        
+        self.catogories = [0, 255] # values of labels in the iuput images of mapping
+        # self.catogories = [1, 2] # 1 for crosswalk, 2 for road
         
         self.preprocessing()
 
+
     def preprocessing(self):
+        """ setup constant matrices """
         self.T_velodyne_to_basklink = self.set_velodyne_to_baselink()
         self.T_cam_to_base = np.matmul(self.T_velodyne_to_basklink, self.cam6.T)
         self.discretize_matrix_inv = np.array([
-            [self.d, 0, 0],
-            [0, -self.d, self.boundary[1][1]/2],
+            [self.d, 0, self.map_boundary[0][0]],
+            [0, -self.d, self.map_boundary[1][1]],
             [0, 0, 1]
         ]).astype(np.float)
         self.discretize_matrix = np.linalg.inv(self.discretize_matrix_inv)
 
+
     def set_velodyne_to_baselink(self):
+        rospy.logwarn("velodyne to baselink from TF is different from this, which is correct?")
         T = euler_matrix(0., 0.157, 0.)
         t = np.array([[2.64, 0, 1.98]]).T
         T[0:3,-1::] = t
         return T
     
+
     def pcd_callback(self, msg):
         rospy.logdebug("pcd data received")
         rospy.logdebug("pcd size: %d, %d", msg.height, msg.width)
@@ -110,9 +118,8 @@ class SemanticMapping:
         ## =========== Mapping
         self.mapping(image_in, self.pose)
 
-    def mapping(self, im_src, pose):
-        self.pose = pose
 
+    def mapping(self, im_src, pose):
         # check if we need to move the local map
         if self.require_new_map(pose):
             pose_old = self.map_pose
@@ -125,18 +132,9 @@ class SemanticMapping:
         pcd_in_range, pcd_label = self.project_pcd(self.pcd, im_src, pose)
         updated_map, normalized_map = self.update_map(self.map, pcd_in_range, pcd_label)
 
-        show_image_list([im_src, normalized_map], delay=1, size=[400, 600])
+        show_image_list([im_src, normalized_map], delay=1, size=[500, 700])
         self.map = updated_map
     
-
-    def get_extrinsics(self, pose):
-        T_base_to_origin = get_transform_from_pose(pose)
-
-        # from camera to origin
-        T_cam_to_origin = np.matmul(T_base_to_origin, self.T_cam_to_base)
-        T_origin_to_cam = np.linalg.inv(T_cam_to_origin)
-        extrinsics = T_origin_to_cam[0:3]
-        return extrinsics
 
     def project_pcd(self, pcd, image, pose):
         """ extract labels of each point in the pcd from image 
@@ -153,7 +151,6 @@ class SemanticMapping:
         # shuffle = np.random.permutation(pcd.shape[1])[0:30]
         # pcd = pcd[:,shuffle]
         pcd = pcd[:,pcd[0,:]!=0]
-
         
         # from base_link to origin (assumed 0,0,0)
         translation = ( pose.position.x, pose.position.y, pose.position.z)
@@ -190,6 +187,7 @@ class SemanticMapping:
         
         return masked_pcd, label
 
+
     def require_new_map(self, pose):
         transform_matrix, trans, rot, euler = get_transformation(tf_listener=self.tf_listener_, tf_ros=self.tf_ros)
         if trans is None or np.abs(trans[0]) > 10 or np.abs(trans[1]) > 2:
@@ -199,9 +197,11 @@ class SemanticMapping:
         rospy.logdebug("%s", "True" if flag else "False")
         return flag
 
+
     def create_new_local_map(self, pose):
         map_new = np.zeros((self.map_height , self.map_width, self.map_depth))
         return map_new
+
 
     def transform_old_map(self, map_new, map_old, pose_old, pose_new):
         rospy.logwarn("Not transforming old map currently")
@@ -224,23 +224,37 @@ class SemanticMapping:
         pcd_pixel = np.matmul(self.discretize_matrix, homogenize(pcd_on_map[0:2,:])).astype(np.int32)
         mask = np.logical_and( np.logical_and(0 <= pcd_pixel[0,:], pcd_pixel[0,:] < self.map_width ),
                                np.logical_and(0 <= pcd_pixel[1,:], pcd_pixel[1,:] < self.map_height ))
-        catogories = [0,255]
-        for i in range(len(catogories)):
-            idx = label[:] == catogories[i]
+        
+        # update corresponding labels
+        for i in range(len(self.catogories)):
+            idx = label[:] == self.catogories[i]
             idx_mask = np.logical_and(idx, mask)
             map_local[pcd_pixel[1, idx_mask], pcd_pixel[0, idx_mask], i] += 1
             print(i, ":", np.sum(map_local[:,:,i]))
+        
+        # threshold and normalize map
         map_local[map_local > self.map_value_max] = self.map_value_max
         print("max:", np.max(map_local))
         normalized_map = self.normalize_map(map_local)
+
         return map_local, normalized_map
-        
+
+
     def normalize_map(self, map_local):
         normalized_map = np.array(map_local)
         normalized_map = normalized_map * 255 / self.map_value_max
         normalized_map = normalized_map.astype(np.uint8)
         return normalized_map
 
+
+    def get_extrinsics(self, pose):
+        T_base_to_origin = get_transform_from_pose(pose)
+
+        # from camera to origin
+        T_cam_to_origin = np.matmul(T_base_to_origin, self.T_cam_to_base)
+        T_origin_to_cam = np.linalg.inv(T_cam_to_origin)
+        extrinsics = T_origin_to_cam[0:3]
+        return extrinsics
 
 # main
 def main():
