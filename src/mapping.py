Fixed a bug in the confusion matrix according to the theoretical formula
* Add logger into the test
* Add the range max control of the point cloud

src/mapping.py
@@ -99,6 +99,7 @@ class SemanticMapping:
         self.pcd_queue = []
         self.pcd_header_queue = []
         self.pcd_time = None
+        self.pcd_range_max = cfg.MAPPING.PCD.RANGE_MAX
         self.use_pcd_intensity = cfg.MAPPING.PCD.USE_INTENSITY
 
         # The coordinate of the map is defined as map[x, y]
@@ -122,10 +123,12 @@ class SemanticMapping:
         # This is a testing parameter, when the time stamp reach this number, the entire node will terminate.
         self.test_cut_time = cfg.TEST_END_TIME
 
-        # confusion_matrix = ConfusionMatrix(load_path=cfg.MAPPING.CONFUSION_MTX.LOAD_PATH)
-        # self.confusion_matrix = confusion_matrix.get_submatrix(cfg.LABELS, to_probability=True, use_log=True)
-        # Use Identity confusion matrix
-        self.confusion_matrix = np.eye(len(self.label_names))
+        if cfg.MAPPING.CONFUSION_MTX.LOAD_PATH != "":
+            confusion_matrix = ConfusionMatrix(load_path=cfg.MAPPING.CONFUSION_MTX.LOAD_PATH)
+            self.confusion_matrix = confusion_matrix.get_submatrix(cfg.LABELS, to_probability=True, use_log=True)
+        else:
+            # Use Identity confusion matrix
+            self.confusion_matrix = np.eye(len(self.label_names))
 
         # Print the configuration to user
         self.logger.log("Running with configuration:\n" + str(cfg))
@@ -222,8 +225,8 @@ class SemanticMapping:
         """ global map origin is shifted to the min x, y point in the point map
         so that the entire map will have positive values """
         pose = Pose()
-        pose.position.x = -1369.0496826171875 # min x
-        pose.position.y = -562.84814453125 # min y
+        pose.position.x = -1369.0496826171875  # min x
+        pose.position.y = -562.84814453125  # min y
         pose.position.z = 0.0
         pose.orientation.w = 1.0
         set_map_pose(pose, '/world', 'global_map')
@@ -314,17 +317,15 @@ class SemanticMapping:
             self.map = self.update_map_planar(self.map, semantic_image, camera_calibration)
 
         if self.save_map_to_file:
-            # color_map = render_bev_map(self.map, self.label_names, self.label_colors)
-            
             # np.save(osp.join(output_dir, "map.npy"), self.map)
-            
-            self.map = apply_filter(self.map) # smooth the labels to fill black holes
+            self.map = apply_filter(self.map)  # smooth the labels to fill black holes
 
-            color_map = render_bev_map_with_thresholds(self.map, self.label_colors, priority=[3, 4, 0, 2, 1],
-                                                       thresholds=[0.1, 0.1, 0.5, 0.20, 0.05])
+            color_map = render_bev_map(self.map, self.label_colors)
+            # color_map = render_bev_map_with_thresholds(self.map, self.label_colors, priority=[3, 4, 0, 2, 1],
+            #                                            thresholds=[0.1, 0.1, 0.5, 0.20, 0.05])
 
             output_dir = self.output_dir
-            makedirs(output_dir, exist_ok=True)            
+            makedirs(output_dir, exist_ok=True)
 
             output_file = osp.join(output_dir, "global_map.png")
             print("Saving image to", output_file)
@@ -332,7 +333,7 @@ class SemanticMapping:
 
             # evaluate
             if self.ground_truth_dir != "":
-                test = Test(ground_truth_dir=self.ground_truth_dir)
+                test = Test(ground_truth_dir=self.ground_truth_dir, logger=self.logger)
                 test.test_single_map(color_map)
 
             # Publish the image
@@ -365,7 +366,10 @@ class SemanticMapping:
 
         IXY = dehomogenize(np.matmul(camera_calibration.P, pcd_velodyne)).astype(np.int32)
 
-        mask_positive = pcd_velodyne[0, :] > 0  # Only use the points in the front.
+        # Only use the points in the front.
+        mask_positive = np.logical_and(0 < pcd_velodyne[0, :], pcd_velodyne[0, :] < self.pcd_range_max)
+
+        # Only select the points that project to the image
         mask = np.logical_and(np.logical_and(0 <= IXY[0, :], IXY[0, :] < image.shape[1]),
                               np.logical_and(0 <= IXY[1, :], IXY[1, :] < image.shape[0]))
         mask = np.logical_and(mask, mask_positive)
@@ -418,7 +422,7 @@ class SemanticMapping:
 
             # Update the local map with Bayes update rule
             # map[pcd_pixel[0, idx_mask], pcd_pixel[1, idx_mask], :] has shape (n, num_classes)
-            map[pcd_pixel[0, idx_mask], pcd_pixel[1, idx_mask], :] += self.confusion_matrix[i, :].reshape(1, -1)
+            map[pcd_pixel[0, idx_mask], pcd_pixel[1, idx_mask], :] += self.confusion_matrix[:, i].reshape(1, -1)
 
             # LiDAR intensity augmentation
             if not self.use_pcd_intensity: continue

test/test_semantic_mapping.py
@@ -14,17 +14,18 @@ def convert_labels(gmap):
     global_map[np.all(gmap == np.array([107, 142, 35]), axis=-1)] = 5  # vegetation
     return global_map
 
+
 def read_img(global_map_path):
     """ read the global map file and covert colors to labels """
     global_map = cv2.imread(global_map_path)
     # gmap = np.rot90(gmap, k=1, axes=(0, 1))
     global_map = convert_labels(global_map)
-    
+
     return gmap, global_map
 
 
 class Test:
-    def __init__(self, ground_truth_dir="./", shift_h = 0, shift_w = 0):
+    def __init__(self, ground_truth_dir="./", shift_h=0, shift_w=0, logger=None):
         """
             Load the ground truth map and do transformations for it. Preprocess and store it for faster testing.
             ground_truth_dir: dir path to ground truth map
@@ -54,6 +55,7 @@ class Test:
         self.class_lists = [1, 2]
         self.shift_w = shift_w
         self.shift_h = shift_h
+        self.logger = logger
 
     def full_test(self, dir_path="./global_maps", visualize=False):
         """
@@ -84,7 +86,7 @@ class Test:
         """
         generate_map = convert_labels(global_map)
         gmap = self.ground_truth_mask[self.shift_w:generate_map.shape[0] + self.shift_w,
-                   self.shift_h:generate_map.shape[1] + self.shift_h]
+               self.shift_h:generate_map.shape[1] + self.shift_h]
         self.iou(gmap, generate_map)
 
     def iou(self, gmap, generate_map):
@@ -107,11 +109,12 @@ class Test:
         miss = 1 - np.sum(np.logical_and((gmap > 0), (generate_map > 0))) / float(np.sum(gmap > 0))
         accuracy = np.sum((gmap == generate_map)[gmap > 0]) / float(np.sum(gmap > 0))
 
-        print("IOU for {}: {}\t{}: {}\tmIOU: {}".format(self.d[0], iou_lists[0], self.d[1], iou_lists[1],
-                                                        np.mean(iou_lists)))
-        print("Accuracy for {}: {}\t{}: {}\tmean Accuracy: {}".format(self.d[0], acc_lists[0], self.d[1], acc_lists[1],
-                                                                      accuracy))
-        print("Overall Missing rate: {}".format(miss))
+        if self.logger:
+            self.logger.log("IOU for {}: {}\t{}: {}\tmIOU: {}".format(self.d[0], iou_lists[0],
+                                                                      self.d[1], iou_lists[1], np.mean(iou_lists)))
+            self.logger.log("Accuracy for {}: {}\t{}: {}\tmean Accuracy: {}".format(self.d[0], acc_lists[0],
+                                                                                    self.d[1], acc_lists[1], accuracy))
+            self.logger.log("Overall Missing rate: {}".format(miss))
 
     def imshow(self, img1, img2):
         fig, axes = plt.subplots(1, 2)
@@ -122,7 +125,7 @@ class Test:
 
 if __name__ == "__main__":
 
-    visualize = False # default to no visualization
+    visualize = False  # default to no visualization
     import sys
 
     # add arguement -v for visualization